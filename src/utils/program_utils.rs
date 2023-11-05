54eafbaac4bb6d3d8cdca7ccd78fb3d036ec50d1
Lab Rat <dev.frolics@gmail.com>
2023-11-05 06:46:17 +0530

    feat: language runner done!

--- src/utils/program_utils.rs
+++ src/utils/program_utils.rs
@@ -1,4 +1,4 @@
-use std::process::{Command, Stdio};
+use std::process::{Command, exit, ExitStatus, Stdio};
 use std::{io, io::Write};
 use which::which;
 
@@ -6,7 +6,12 @@ fn program_exists(program: &str) -> Result<std::path::PathBuf, which::Error> {
     which(program)
 }
 
-fn run_program(program: &str, args: Vec<&str>, stdin_content: String) -> io::Result<String> {
+pub fn run_program_with_input(program: &str, args: &Vec<&str>, stdin_content: &str) -> io::Result<String> {
+
+    if let Err(err) = program_exists(program) {
+        return Err(io::Error::new(io::ErrorKind::Other, err));
+    }
+
     let mut child = Command::new(program)
         .args(args)
         .stdin(Stdio::piped())
@@ -21,6 +26,44 @@ fn run_program(program: &str, args: Vec<&str>, stdin_content: String) -> io::Res
     let output = child.wait_with_output()?;
 
     let stdout_content = String::from_utf8(output.stdout)
+        .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+        .expect("Found invalid UTF-8");
+
+    Ok(stdout_content)
+}
+
+pub fn run_program(program: &str, args: &Vec<&str>) -> io::Result<String> {
+
+    if let Err(err) = program_exists(program) {
+        return Err(io::Error::new(io::ErrorKind::Other, err));
+    }
+
+    let mut child = Command::new(program)
+        .args(args)
+        .stdout(Stdio::piped())
+        .output();
+
+    let child = match child {
+        Ok(t) => t,
+        Err(err) => {
+            eprintln!("Failed to run the command {} {:?}", program, args);
+            return Err(io::Error::new(io::ErrorKind::Other, err))
+        }
+    };
+
+    if child.status.code() != Some(0) {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!(
+                "Process failed to run successfully!\nStatus Code: {}\n Output: {}\nError: {}",
+                child.status,
+                String::from_utf8_lossy(&child.stdout),
+                String::from_utf8_lossy(&child.stderr)
+            )
+        ));
+    }
+
+    let stdout_content = String::from_utf8(child.stdout)
         .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
         .expect("Found invalid UTF-8");
 
