241049e2d9089f6b2c903c29492e7761fde3dc64
Lab Rat <dev.frolics@gmail.com>
2023-11-11 10:29:11 +0530

    feat: single time compilation support and improved docs

--- src/program_store.rs
+++ src/program_store.rs
@@ -1,51 +1,46 @@
-use std::path::Path;
-use std::process::exit;
-use clap::builder::Str;
 use crate::language::Language;
 use crate::utils::file_utils;
+use std::path::Path;
+use std::process::exit;
 
 const FILE_NOT_FOUND_EXIT_CODE: i32 = 6;
 
 #[derive(Debug)]
-pub(crate) struct ProgramStore<'a> {
-    source_file: &'a Path,
-    test_file: &'a Path,
-    source_lang: Option<Language>,
-    test_lang: Option<Language>
+pub(crate) struct ProgramStore {
+    correct_file: Language,
+    test_file: Language,
 }
 
-impl<'a> ProgramStore<'a> {
-    pub fn new(source_file: &'a Path, comparing_file: &'a Path) -> ProgramStore<'a> {
+impl ProgramStore {
+    pub fn new(source_file: &Path, comparing_file: &Path) -> ProgramStore {
         Self {
-            source_file,
-            test_file: comparing_file,
-            source_lang: Language::get_programming_language_name(source_file),
-            test_lang: Language::get_programming_language_name(comparing_file),
+            correct_file: Language::new(source_file),
+            test_file: Language::new(comparing_file),
         }
     }
 
     fn exists(&self) -> bool {
-        self.source_file.exists() && self.test_file.exists()
+        self.correct_file.file_path.exists() && self.test_file.file_path.exists()
     }
 
-    pub fn run_code(&self, stdin_content: &str) -> Result<(bool, String, String), &str> {
+    pub fn run_code(&mut self, stdin_content: &str) -> Result<(bool, String, String), &str> {
         if !self.exists() {
             eprintln!("[ERROR] File(s) doesn't exists\nQuitting.....");
             exit(FILE_NOT_FOUND_EXIT_CODE);
         }
-        let src_file = Language::run_program_code(self.source_file, &self.source_lang, stdin_content);
-        let test_file = Language::run_program_code(self.test_file, &self.test_lang, stdin_content);
+        let src_file = self.correct_file.run_program_code(stdin_content);
+        let test_file = self.test_file.run_program_code(stdin_content);
 
         match src_file {
-            Ok(src_output) => {
-                match test_file  {
-                    Ok(test_output) => {
-                        Ok((file_utils::string_diff(&src_output, &test_output), src_output, test_output))
-                    }
-                    Err(err) => {
-                        eprintln!("Failed to run test file!\n{err}");
-                        Err("Error running Test File")
-                    }
+            Ok(src_output) => match test_file {
+                Ok(test_output) => Ok((
+                    file_utils::string_diff(&src_output, &test_output),
+                    src_output,
+                    test_output,
+                )),
+                Err(err) => {
+                    eprintln!("Failed to run test file!\n{err}");
+                    Err("Error running Test File")
                 }
             },
             Err(err) => {
