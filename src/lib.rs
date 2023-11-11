96190acc5c82edf6a31e6d7da3dd3926c26df7fb
Lab Rat <dev.frolics@gmail.com>
2023-11-11 10:29:11 +0530

    feat: single time compilation support and improved docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,79 @@
 #![allow(unused)]
 
+//! # cpast - Code Testing and Analysis Tool
+//!
+//! `cpast` is a versatile code testing and analysis tool that empowers users in competitive programming and coding practice. It allows testing correct and incorrect code files against a custom language generator called `clex`. This crate supports various programming languages, such as Python, C++, C, Rust, Ruby, JavaScript, and Java, and enables users to specify the number of iterations for testing code against random input values.
+//!
+//! ## Main Modules
+//!
+//! - `language`: Module for language-runner-related functionalities.
+//! - `program_store`: Module handling the storage and management of code programs.
+//! - `utils`: Utility module with miscellaneous functions.
+//! - `clex_language`: Module containing lexer, parser, generator, and abstract syntax tree (AST) for the custom language `clex`.
+//!
+//! ## Introduction
+//!
+//! The crate provides solutions to common challenges faced by competitive programmers and coding enthusiasts, such as verifying code correctness, efficient testing under time constraints, and quick debugging to improve code performance.
+//!
+//! ## Usage
+//!
+//! To get started with `cpast`, users can use the provided functions:
+//!
+//! - `compile_and_test`: Compiles and tests code against a custom language generator.
+//! - `get_tokens`: Retrieves tokens from the custom language lexer.
+//! - `get_ast`: Obtains the Abstract Syntax Tree (AST) from the custom language parser.
+//! - `generator`: Generates code based on the custom language specification.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use cpast::{compile_and_test, get_tokens, get_ast, generator};
+//!
+//! compile_and_test("correct.cpp".to_string(), "incorrect.cpp".to_string(), "(N) (?:N){\\1}".to_string(), 100);
+//!
+//! let tokens = get_tokens("(N) (?:N){\\1}".to_string());
+//!
+//! let ast = get_ast("(N) (?:N){\\1}".to_string());
+//!
+//! let generated_code = generator("(N) (?:N){\\1}".to_string());
+//! ```
+//!
+//! For more details on usage and advanced features, refer to the README.
+//!
+
+mod clex_language;
 mod language;
 mod program_store;
 mod utils;
-mod clex_language;
 
 use std::path::Path;
 use std::process::exit;
 
+use crate::clex_language::lexer::Token;
+use crate::clex_language::{ast::Program, generator, lexer, parser};
 use crate::program_store::ProgramStore;
-use crate::clex_language::{lexer, parser, generator};
 
-pub fn compile_and_test(src_binding:String, test_binding: String, language: String, iterations: usize) {
-
-    let store = ProgramStore::new(
-        Path::new(&src_binding),
-        Path::new(&test_binding)
-    );
+/// Compile and test code against custom language generator.
+///
+/// # Arguments
+///
+/// * `correct_binding` - The source code file path containing correct code.
+/// * `test_binding` - The source code file path containing incorrect code for testing.
+/// * `language` - The custom language generator code for test generation.
+/// * `iterations` - The number of test iterations to run.
+///
+/// # Example
+///
+/// ```rust
+/// cpast::compile_and_test("correct.cpp".to_string(), "incorrect.cpp".to_string(), "(N) (?:N){\\1}".to_string(), 100);
+/// ```
+pub fn compile_and_test(
+    correct_binding: String,
+    test_binding: String,
+    language: String,
+    iterations: usize,
+) {
+    let mut store = ProgramStore::new(Path::new(&correct_binding), Path::new(&test_binding));
 
     let mut token = lexer::Tokens::new(language);
     token.scan_tokens();
@@ -39,11 +96,74 @@ pub fn compile_and_test(src_binding:String, test_binding: String, language: Stri
                 println!("==============================");
                 println!("ACTUAL OUTPUT\n{}", actual);
                 exit(1);
-            },
-            Err(err) => println!("Error matching the file! {}", err)
+            }
+            Err(err) => println!("Error matching the file! {}", err),
         }
-
 
         gen.reset_output();
     }
+}
+
+/// Get tokens from the custom language lexer.
+///
+/// # Arguments
+///
+/// * `language` - The custom language generator code for test generation.
+///
+/// # Returns
+///
+/// A vector of `Token` representing the lexed tokens.
+///
+/// # Example
+///
+/// ```rust
+/// let tokens = cpast::get_tokens("(N) (?:N){\\1}".to_string());
+/// ```
+pub fn get_tokens(language: String) -> Vec<Token> {
+    let mut token = lexer::Tokens::new(language);
+    token.scan_tokens();
+    token.tokens
+}
+
+/// Get the Abstract Syntax Tree (AST) from the custom language parser.
+///
+/// # Arguments
+///
+/// * `language` - The custom language generator code for test generation.
+///
+/// # Returns
+///
+/// The `Program` AST representing the parsed program.
+///
+/// # Example
+///
+/// ```rust
+/// let ast = cpast::get_ast("(N) (?:N){\\1}".to_string());
+/// ```
+pub fn get_ast(language: String) -> Program {
+    let mut parser = parser::Parser::new(language);
+    parser.parser();
+    parser.language
+}
+
+/// Generate code based on the custom language specification.
+///
+/// # Arguments
+///
+/// * `language` - The custom language generator code for test generation.
+///
+/// # Returns
+///
+/// A string representing the generated test pattern.
+///
+/// # Example
+///
+/// ```rust
+/// let generated_code = cpast::generator("(N) (?:N){\\1}".to_string());
+/// ```
+pub fn generator(language: String) -> String {
+    let parser = parser::Parser::new(language);
+    let mut gen = generator::Generator::new(parser);
+    gen.traverse_ast();
+    gen.output_text
 }
