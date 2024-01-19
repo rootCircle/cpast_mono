a37e03f9c931aa3a33442393035258c0f3a7dd94
Lab Rat <dev.frolics@gmail.com>
2024-01-20 00:59:51 +0530

    docs: added examples and updated video

---
+++ examples/test.rs
@@ -0,0 +1,19 @@
+use std::{path::PathBuf, env};
+
+use cpast::compile_and_test;
+
+fn main() {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+
+    // Just to ensure that compiled binaries are in examples dir
+    env::set_current_dir(PathBuf::from(format!("{manifest_dir}/examples/artifacts"))).unwrap();
+
+    compile_and_test(
+        format!("{manifest_dir}/examples/res/correct_approach.cpp"),
+        format!("{manifest_dir}/examples/res/my_approach.cpp"),
+        "(N[1,5]) (?:(N[1,5]) (?:N[1,100]){\\2}){\\1}".to_owned(),
+        100,
+        true,
+        false,
+    );
+}
